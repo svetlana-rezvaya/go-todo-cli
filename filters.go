700258e2a2329af7a3d58f83cd57fc6b68a0b726
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-12-08 21:17:54 +0300

    Add filterByText function

--- filters.go
+++ filters.go
@@ -1,9 +1,22 @@
 package main
 
+import "strings"
+
 func filterByStatus(notes []note, status bool) []note {
 	notesCopy := []note{}
 	for _, note := range notes {
 		if note.IsDone == status {
+			notesCopy = append(notesCopy, note)
+		}
+	}
+
+	return notesCopy
+}
+
+func filterByText(notes []note, text string) []note {
+	notesCopy := []note{}
+	for _, note := range notes {
+		if strings.Contains(note.Text, text) {
 			notesCopy = append(notesCopy, note)
 		}
 	}
--- filters_test.go
+++ filters_test.go
@@ -52,3 +52,32 @@ func Test_filterByStatus_isNotDone(test *testing.T) {
 		test.Fail()
 	}
 }
+
+func Test_filterByText_empty(test *testing.T) {
+	notes := filterByText([]note{}, "No.2")
+
+	wantedNotes := []note{}
+	if !reflect.DeepEqual(notes, wantedNotes) {
+		test.Fail()
+	}
+}
+
+func Test_filterByText_nonEmpty(test *testing.T) {
+	notes := []note{
+		note{ID: 100, IsDone: false, Text: "task No.1, one"},
+		note{ID: 101, IsDone: true, Text: "task No.1, two"},
+		note{ID: 102, IsDone: false, Text: "task No.2, three"},
+		note{ID: 103, IsDone: true, Text: "task No.2, four"},
+		note{ID: 104, IsDone: false, Text: "task No.2, five"},
+	}
+	notes = filterByText(notes, "No.2")
+
+	wantedNotes := []note{
+		note{ID: 102, IsDone: false, Text: "task No.2, three"},
+		note{ID: 103, IsDone: true, Text: "task No.2, four"},
+		note{ID: 104, IsDone: false, Text: "task No.2, five"},
+	}
+	if !reflect.DeepEqual(notes, wantedNotes) {
+		test.Fail()
+	}
+}
