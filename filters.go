3412b9f984d92f4df921c25d661d59bee7a38fc8
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-12-13 21:51:50 +0300

    Add filterByTime function

--- filters.go
+++ filters.go
@@ -1,6 +1,9 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"time"
+)
 
 func filterByStatus(notes []note, status bool) []note {
 	notesCopy := []note{}
@@ -17,6 +20,19 @@ func filterByText(notes []note, text string) []note {
 	notesCopy := []note{}
 	for _, note := range notes {
 		if strings.Contains(note.Text, text) {
+			notesCopy = append(notesCopy, note)
+		}
+	}
+
+	return notesCopy
+}
+
+func filterByTime(notes []note, minimum time.Time, maximum time.Time) []note {
+	notesCopy := []note{}
+	for _, note := range notes {
+		tooEarly := note.CreatedAt.Before(minimum)
+		tooLate := note.CreatedAt.After(maximum)
+		if !tooEarly && !tooLate {
 			notesCopy = append(notesCopy, note)
 		}
 	}
--- filters_test.go
+++ filters_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func Test_filterByStatus_empty(test *testing.T) {
@@ -76,6 +77,64 @@ func Test_filterByText_nonEmpty(test *testing.T) {
 		note{ID: 102, IsDone: false, Text: "task No.2, three"},
 		note{ID: 103, IsDone: true, Text: "task No.2, four"},
 		note{ID: 104, IsDone: false, Text: "task No.2, five"},
+	}
+	if !reflect.DeepEqual(notes, wantedNotes) {
+		test.Fail()
+	}
+}
+
+func Test_filterByTime_empty(test *testing.T) {
+	minimum := time.Date(2020, time.December, 9, 21, 5, 20, 123, time.UTC)
+	maximum := minimum.Add(567 * time.Hour)
+	notes := filterByTime([]note{}, minimum, maximum)
+
+	wantedNotes := []note{}
+	if !reflect.DeepEqual(notes, wantedNotes) {
+		test.Fail()
+	}
+}
+
+func Test_filterByTime_nonEmpty(test *testing.T) {
+	createdAt := time.Date(2020, time.December, 9, 21, 5, 20, 123, time.UTC)
+	minimum := createdAt.Add(2 * time.Hour)
+	maximum := createdAt.Add(3 * time.Hour)
+	notes := []note{
+		note{ID: 100, CreatedAt: createdAt, IsDone: false, Text: "one"},
+		note{ID: 101, CreatedAt: createdAt.Add(time.Hour), IsDone: true, Text: "two"},
+		note{
+			ID:        102,
+			CreatedAt: createdAt.Add(2 * time.Hour),
+			IsDone:    false,
+			Text:      "three",
+		},
+		note{
+			ID:        103,
+			CreatedAt: createdAt.Add(3 * time.Hour),
+			IsDone:    true,
+			Text:      "four",
+		},
+		note{
+			ID:        104,
+			CreatedAt: createdAt.Add(4 * time.Hour),
+			IsDone:    false,
+			Text:      "five",
+		},
+	}
+	notes = filterByTime(notes, minimum, maximum)
+
+	wantedNotes := []note{
+		note{
+			ID:        102,
+			CreatedAt: createdAt.Add(2 * time.Hour),
+			IsDone:    false,
+			Text:      "three",
+		},
+		note{
+			ID:        103,
+			CreatedAt: createdAt.Add(3 * time.Hour),
+			IsDone:    true,
+			Text:      "four",
+		},
 	}
 	if !reflect.DeepEqual(notes, wantedNotes) {
 		test.Fail()
