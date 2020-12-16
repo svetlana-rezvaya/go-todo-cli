df69e8ad1948c06586b156122b9ed3210f9846e2
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-12-16 20:50:31 +0300

    Add failure tests for unmarshalNote function

--- unmarshal_notes_test.go
+++ unmarshal_notes_test.go
@@ -47,3 +47,49 @@ func Test_unmarshalNote_successAndIsDone(test *testing.T) {
 		test.Fail()
 	}
 }
+
+func Test_unmarshalNote_errorWithPartCount(test *testing.T) {
+	noteObject, err := unmarshalNote("10123 [_] 09 Dec 20")
+
+	wantedNoteObject := note{}
+	if !reflect.DeepEqual(noteObject, wantedNoteObject) {
+		test.Fail()
+	}
+
+	if err == nil || err.Error() != "not enough parts in the line" {
+		test.Fail()
+	}
+}
+
+func Test_unmarshalNote_errorWithIncorrectID(test *testing.T) {
+	noteObject, err :=
+		unmarshalNote("incorrect [_] 09 Dec 20 21:05 +0000 one two three")
+
+	wantedNoteObject := note{}
+	if !reflect.DeepEqual(noteObject, wantedNoteObject) {
+		test.Fail()
+	}
+
+	wantedErrStr := "unable to parse the note ID: " +
+		"strconv.Atoi: parsing \"incorrect\": invalid syntax"
+	if err == nil || err.Error() != wantedErrStr {
+		test.Fail()
+	}
+}
+
+func Test_unmarshalNote_errorWithIncorrectTimestamp(test *testing.T) {
+	noteObject, err :=
+		unmarshalNote("10123 [_] 09 12 20 21:05 +0000 one two three")
+
+	wantedNoteObject := note{}
+	if !reflect.DeepEqual(noteObject, wantedNoteObject) {
+		test.Fail()
+	}
+
+	wantedErrStr := "unable to parse the note creation timestamp: " +
+		"parsing time \"09 12 20 21:05 +0000\" as \"02 Jan 06 15:04 -0700\": " +
+		"cannot parse \"12 20 21:05 +0000\" as \"Jan\""
+	if err == nil || err.Error() != wantedErrStr {
+		test.Fail()
+	}
+}
