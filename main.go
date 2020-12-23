7502e572324632c847dd270cc6f18abf9b9d5557
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-12-23 20:24:20 +0300

    Add basics of list command

--- main.go
+++ main.go
@@ -5,9 +5,35 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	createdAt := time.Date(2020, time.December, 9, 21, 5, 20, 123, time.UTC)
+	notes := []note{
+		note{ID: 100, CreatedAt: createdAt, IsDone: false, Text: "one"},
+		note{ID: 101, CreatedAt: createdAt.Add(time.Hour), IsDone: true, Text: "two"},
+		note{
+			ID:        102,
+			CreatedAt: createdAt.Add(2 * time.Hour),
+			IsDone:    false,
+			Text:      "three",
+		},
+		note{
+			ID:        103,
+			CreatedAt: createdAt.Add(3 * time.Hour),
+			IsDone:    true,
+			Text:      "four",
+		},
+		note{
+			ID:        104,
+			CreatedAt: createdAt.Add(4 * time.Hour),
+			IsDone:    false,
+			Text:      "five",
+		},
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("> ")
@@ -18,6 +44,10 @@ func main() {
 			continue
 		}
 
-		fmt.Print(line)
+		line = strings.TrimSpace(line)
+		if line == "list" {
+			text := marshalNotes(notes)
+			fmt.Print(text)
+		}
 	}
 }
