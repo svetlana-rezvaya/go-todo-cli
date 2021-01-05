2c8a88a4ddc97b68cf7a5aaf7c009829af65c8ee
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-01-05 11:48:14 +0300

    Add loadNotes function

--- storage.go
+++ storage.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 )
+
+func loadNotes(storageFilename string) ([]note, error) {
+	file, err := os.Open(storageFilename)
+	if err != nil {
+		return nil, errors.New("unable to open a storage file: " + err.Error())
+	}
+	defer file.Close()
+
+	textBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, errors.New("unable to read a storage file: " + err.Error())
+	}
+
+	notes, err := unmarshalNotes(string(textBytes))
+	if err != nil {
+		return nil, errors.New("unable to unmarshal a storage file: " + err.Error())
+	}
+
+	return notes, nil
+}
 
 func saveNotes(storageFilename string, notes []note) error {
 	file, err := os.Create(storageFilename)
