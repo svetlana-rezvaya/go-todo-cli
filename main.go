65c28de90bafee1aec52eac34e823b83d6a860a1
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-01-05 11:55:19 +0300

    Use loadNotes function

--- main.go
+++ main.go
@@ -11,28 +11,9 @@ import (
 )
 
 func main() {
-	createdAt := time.Date(2020, time.December, 9, 21, 5, 20, 123, time.UTC)
-	notes := []note{
-		note{ID: 100, CreatedAt: createdAt, IsDone: false, Text: "one"},
-		note{ID: 101, CreatedAt: createdAt.Add(time.Hour), IsDone: true, Text: "two"},
-		note{
-			ID:        102,
-			CreatedAt: createdAt.Add(2 * time.Hour),
-			IsDone:    false,
-			Text:      "three",
-		},
-		note{
-			ID:        103,
-			CreatedAt: createdAt.Add(3 * time.Hour),
-			IsDone:    true,
-			Text:      "four",
-		},
-		note{
-			ID:        104,
-			CreatedAt: createdAt.Add(4 * time.Hour),
-			IsDone:    false,
-			Text:      "five",
-		},
+	notes, err := loadNotes("storage.data")
+	if err != nil {
+		log.Fatal("unable to load notes: ", err)
 	}
 
 	reader := bufio.NewReader(os.Stdin)
